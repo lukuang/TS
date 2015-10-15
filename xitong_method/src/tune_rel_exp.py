debb27e45e07f6c9cf40ed2ec99262b81780f9fc
Kuang Lu <lukuang@n088.farber.hpc.udel.edu>
2015-10-15 09:44:34 -0400

    xitong method - change tune_rel_exp.pl so that it can be used to perform both entity based and relation based expansion

--- xitong_method/src/tune_rel_exp.py
+++ xitong_method/src/tune_rel_exp.py
@@ -1,9 +1,13 @@
 """
-generate multiple expansions for rel-exp-query-v2.pl
+generate multiple expansions for different expansion methods
+
+src/ent-exp-query-v3.pl : entity name based expansion
+src/rel-exp-query-v2.pl : entity relation based expansion
 """
 import os,sys
 
-dest_dir = sys.argv[1]
+program_file = sys.argv[1]
+dest_dir = sys.argv[2]
 model = "/lustre/scratch/lukuang/Temporal_Summerization/TS/xitong_method/data/query_ml"
 entity_list = "/lustre/scratch/lukuang/Temporal_Summerization/TS/xitong_method/res/ent/text.txt"
 ent_num = [1,3,5,7,9,11,13,15];
@@ -12,6 +16,6 @@ lambdas = [0.0,0.2,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
 for num in ent_num:
     for l in lambdas:
         save = os.path.join(dest_dir,str(num)+"-"+str(l))
-        execute = "perl src/rel-exp-query-v2.pl %s %s %s %s %s" %(entity_list, str(num), str(l), model, save)
+        execute = "perl %s %s %s %s %s %s" %(program_file, entity_list, str(num), str(l), model, save)
         print execute
         os.system( execute)
