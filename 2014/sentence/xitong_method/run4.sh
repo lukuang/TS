2d4fa0357add4fc6a779bbeed62ae24a1cc56731
Kuang Lu <lukuang@g115.farber.hpc.udel.edu>
2015-11-16 14:39:27 -0500

    xitong method -  modify the sentence retrieval method

---
+++ 2014/sentence/xitong_method/run4.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Tell the SGE that this is an array job, with "tasks" to be numbered 1 to 10000
+#$ -l standby=1,h_rt=4:00:00
+#$ -l m_mem_free=5G
+# -m eas
+# -M lukuang@udel.edu
+#$ -t 23-25
+vpkg_require python
+source /home/1546/myEV/bin/activate
+# When a single command in the array job is sent to a compute node,
+# its task number is stored in the variable SGE_TASK_ID,
+# so we can use the value of that variable to get the results we want:
+python no_sub_collection_select.py test_para $model $SGE_TASK_ID $a $b $c $d
+#echo $SGE_TASK_ID
