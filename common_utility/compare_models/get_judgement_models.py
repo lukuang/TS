e7676178b800884b574e0111904137553f37d1b9
lukuang <lukuang1989@gmail.com>
2015-11-25 11:35:34 -0500

    model compare - bug fixing for  get_judgement_models.py

--- common_utility/compare_models/get_judgement_models.py
+++ common_utility/compare_models/get_judgement_models.py
@@ -92,12 +92,12 @@ class GoldModels(object):
             self.get_update_id(self,qid)
             with open(self._update_file) as f:
                 parts = line.rstrip().split()
-                    if parts[0] == qid:
-                        uid = parts[1]
+                if parts[0] == qid:
+                    uid = parts[1]
 
-                        if uid in self._update_ids[qid]:
-                            sentence = parts[6]
-                            update_model(sentence,self._sentence_model[qid])
+                    if uid in self._update_ids[qid]:
+                        sentence = parts[6]
+                        update_model(sentence,self._sentence_model[qid])
 
         return self._sentence_model[qid]
 
