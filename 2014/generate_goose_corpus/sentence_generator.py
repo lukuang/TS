941f6e86fb00845133448a20b93a860d09316763
lukuang <lukuang1989@gmail.com>
2015-12-01 16:29:25 -0500

    goose - fix typo

--- 2014/generate_goose_corpus/sentence_generator.py
+++ 2014/generate_goose_corpus/sentence_generator.py
@@ -27,6 +27,7 @@ class Sentence_generator(object):
     def get_sentences(self,raw_html):
         #get cleaned text
         article = self._g.extract(raw_html = raw_html)
+        print type(text)
         text = article.cleaned_text.decode("utf-8",'ignore')
         #print "clean text is:"
         #print text
