1bdf475985c37832dd9c2c8a44bf3eae7781c243
lukuang <lukuang1989@gmail.com>
2015-12-01 15:51:50 -0500

    goose - fix typo

--- 2014/generate_goose_corpus/sentence_generator.py
+++ 2014/generate_goose_corpus/sentence_generator.py
@@ -27,11 +27,11 @@ class Sentence_generator(object):
     def get_sentences(self,raw_html):
         #get cleaned text
         article = self._g.extract(raw_html = raw_html)
-        text = article.cleaned_text.strip()
+        text = article.cleaned_text.strip().decode("utf-8") 
         print "clean text is:"
         print text
         #get sentences using corenlp
-        nlp_data = json.loads( self._corenlp.parse(text).decode("utf-8") )
+        nlp_data = json.loads( self._corenlp.parse(text))
 
         sentences = [x["text"] for x in nlp_data["sentences"]]
 
