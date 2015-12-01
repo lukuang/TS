cdcf5fcb6bd5bc0c2b6327cd84ec0a26b62381c7
lukuang <lukuang1989@gmail.com>
2015-12-01 16:02:07 -0500

    goose - fix typo

--- 2014/generate_goose_corpus/get_cleaned_corpus.py
+++ 2014/generate_goose_corpus/get_cleaned_corpus.py
@@ -77,6 +77,7 @@ def get_doc(si):
         sentence=""
         for token in sentence_tokens:
             sentence = "%s%s "%(sentence,token.token)
+        print sentence
         document["sentences"][sentence_index_string] = sentence.encode('utf-8')
     document["time"]=int(si.stream_time.epoch_ticks)
 
