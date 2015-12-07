847ca34fc864422082a2109c6347677dd4b733a1
Kuang Lu <lukuang@login000.(none)>
2015-12-07 14:14:20 -0500

    goose - add sentence retrieval files

--- 2014/sentence/goose_corpus/first_sentence/axiomatic/run1.sh
+++ 2014/sentence/goose_corpus/first_sentence/axiomatic/run1.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-#$ -l standby=1
-#$ -l m_mem_free=5G,h_rt=4:00:00
+# Tell the SGE that this is an array job, with "tasks" to be numbered 1 to 10000
+#$ -l standby=1,h_rt=4:00:00
+#$ -l m_mem_free=5G
 # -m eas
 # -M lukuang@udel.edu
-#$ -t 1-2
+#$ -t 14-16
 vpkg_require python
 source /home/1546/myEV/bin/activate
 # When a single command in the array job is sent to a compute node,
 # its task number is stored in the variable SGE_TASK_ID,
 # so we can use the value of that variable to get the results we want:
-echo  $model $qid  $mu $SGE_TASK_ID
-python json_find.py para  $model $qid  $mu $SGE_TASK_ID
+python no_sub_collection_select.py test_para $model $SGE_TASK_ID $a $b $c $d
 #echo $SGE_TASK_ID
--- 2014/sentence/goose_corpus/first_sentence/axiomatic/statistics.py
+++ 2014/sentence/goose_corpus/first_sentence/axiomatic/statistics.py
@@ -3,63 +3,69 @@ from stemming.porter import stem
 import re
 
 class Statistics:
-    def __init__(self, query_model, background, mu):
+    def __init__(self, query_model, background, mu, a, b, sentence_mu, top_percent,\
+        sim_threshold, doc_num, _stopwords):
         self._mu = mu
         self._query_model = query_model
         self._background = background
-        min = 1000
+        self._a = a
+        self._b = b
+        self._sentence_mu = sentence_mu
+        self._top_percent = top_percent
+        self._sim_threshold = sim_threshold
+        self._doc_num = doc_num
+        self._stopwords = _stopwords
+	min = 1000
         for term in background:
-            if background[term] != 0.0 and min > background[term]:
-                min = background[term]
-        print "min is", min
+            if background[term] < min and background[term] != 0.0:
+                 min = background[term]
         for term in self._background:
-            if self._background[term] == 0.0:
-                 self._background[term] = min      
+             if self._background[term] == 0.0:
+                 self._background[term] = min
 
 class Sentence:
-    def __init__(self, sentence_string):
+    def __init__(self, sentence_string, s):
         self._terms = {}
         self._length = 0
         #print "the sentence is:",sentence_string
         for word in re.findall("\w+",sentence_string):
+            self._length += 1
             stem_word = stem(word.lower())
+            if stem_word in s._stopwords:
+                continue
             if stem_word not in self._terms:
                 self._terms[stem_word] = 1
             else:
                 #print stem_word
                 self._terms[stem_word] += 1
-            self._length += 1
+            
         #for t in self._terms:
         #    print t,":",self._terms[t]
         #print "_"*20
 
     
 class Document:
-    def __init__(self, sentences_dict, time, s, query_words):
+    def __init__(self, sentences_dict, time, s):
         terms = {}
         self._model = {}
         self._length = 0
         self._time = time
         self._sentences = {}
-        self._needed = False
         for key in sentences_dict:
             #print "get sentence:", key
             if sentences_dict[key] == None:
                 continue
-            self._sentences[key]=Sentence(sentences_dict[key])
+            self._sentences[key]=Sentence(sentences_dict[key], s)
             for term in self._sentences[key]._terms:
                 if term not in terms:
                     terms[term] = self._sentences[key]._terms[term]
                 else:
                     terms[term] = self._sentences[key]._terms[term] + terms[term]
                 self._length += self._sentences[key]._terms[term]
-        self._needed = False
-        for t in query_words:
-            if t in terms:
-                self._needed  = True
-        if not self._needed:    
-            return None
+
         for term in s._background:
+            if s._background[term] == 0.0:
+                continue
             if term in terms :
                 self._model[term] = (terms[term] + s._mu*s._background[term])/(self._length + s._mu)
             else:
