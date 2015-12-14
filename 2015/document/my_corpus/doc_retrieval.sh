f93d163531d44ce21cf7211cdc83fd116ea9cbf9
Kuang Lu <lukuang@n028.farber.hpc.udel.edu>
2015-12-14 16:18:01 -0500

    cleaned corpus - add scripts for document retrieval for 2015

---
+++ 2015/document/my_corpus/doc_retrieval.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#$ -l standby=1
+#$ -l m_mem_free=5G
+# -m eas
+# -M lukuang@udel.edu
+#$ -t 1
+vpkg_require python
+source /home/1546/myEV/bin/activate
+# When a single command in the array job is sent to a compute node,
+# its task number is stored in the variable SGE_TASK_ID,
+# so we can use the value of that variable to get the results we want:
+echo  $qid  $SGE_TASK_ID
+python json_find.py para  $qid 
+#echo $SGE_TASK_ID
