aff6cc49502ea4111bca4656ce22e5924c68b3cd
lukuang <lukuang1989@gmail.com>
2016-01-13 14:06:12 -0500

    post analysis - add score_threshold run

---
+++ 2015/sentence/my_corpus/score_threshold/score_threshold.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Tell the SGE that this is an array job, with "tasks" to be numbered 1 to 10000
+#$ -l standby=1,h_rt=4:00:00
+#$ -l m_mem_free=1G
+#$ -t 26-46
+vpkg_require python
+source /home/1546/myEV/bin/activate
+# When a single command in the array job is sent to a compute node,
+# its task number is stored in the variable SGE_TASK_ID,
+# so we can use the value of that variable to get the results we want:
+python no_sub_collection_select.py test_para  $SGE_TASK_ID
